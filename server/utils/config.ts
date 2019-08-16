a35dfc13e26fb82f3eb4bf83049a7b9bdca66f92
Christopher D Jativa <cjativa@barnesfoundation.org>
2019-08-15 21:21:09 -0400

    db environment variables

--- server/utils/config.ts
+++ server/utils/config.ts
@@ -5,5 +5,10 @@ dotenv.config();
 
 export const Config = {
     surveyMonkeyUrl: process.env.SURVEY_MONKEY_API_URL,
-    surveyMonkeyAccessToken: process.env.SURVEY_MONKEY_ACCESS_TOKEN
+    surveyMonkeyAccessToken: process.env.SURVEY_MONKEY_ACCESS_TOKEN,
+
+    dbHost: process.env.DB_HOST,
+    dbUser: process.env.DB_USER,
+    dbPassword: process.env.DB_PASSWORD,
+    dbName: process.env.DB_NAME,
 }
